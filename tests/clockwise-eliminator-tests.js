c7352d9d8b200b1624df21a0aa5bfb15c7ae2957
Asheesh Karwal <asheesh.karwal@myob.com>
2017-04-16 16:44:11 +1000

    Adds more specific boundary tests for clockwise eliminator

---
+++ tests/clockwise-eliminator-tests.js
@@ -0,0 +1,36 @@
+const test = require('tape');
+const ClockwiseEliminator = require('../lib/clockwise-eliminator');
+
+test('eliminates items when hop count less than number of items', assert => {
+  const result = clockwiseEliminate([1,2,3], {hopCount: 2});
+  assert.deepEqual(result, [2,1,3]);
+  assert.end();
+});
+
+test('eliminates items when hop count equal to number of items', assert => {
+  const result = clockwiseEliminate([1,2,3], {hopCount: 3});
+  assert.deepEqual(result, [3,1,2]);
+  assert.end();
+});
+
+test('eliminates items when hop count greater than number of items', assert => {
+  const result = clockwiseEliminate([1,2,3], {hopCount: 4});
+  assert.deepEqual(result, [1,3,2]);
+  assert.end();
+});
+
+test('eliminates items when hop count is 1', assert => {
+  const result = clockwiseEliminate([1,2,3], {hopCount: 1});
+  assert.deepEqual(result, [1,2,3]);
+  assert.end();
+});
+
+test('eliminates nothing if no items given', assert => {
+  const result = clockwiseEliminate([], {hopCount: 1});
+  assert.deepEqual(result, []);
+  assert.end();
+});
+
+function clockwiseEliminate(items, settings) {
+  return new ClockwiseEliminator(settings).eliminateAll(items);
+}
