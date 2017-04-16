28a7eb336bbd2e91d250d51060329ac31fbbbeed
Asheesh Karwal <asheesh.karwal@myob.com>
2017-04-16 15:25:32 +1000

    Identifies the last eliminated child as the winner

--- tests/game-tests.js
+++ tests/game-tests.js
@@ -4,7 +4,19 @@ const Game = require('../lib/game');
 test('eliminates children clockwise after the given number of hops', (assert) => {
   const noOfChildren = 5;
   const hopCount = 3;
-  const result = new Game().play({noOfChildren, hopCount});
+  const result = play({noOfChildren, hopCount});
   assert.deepEqual(result.eliminatedSequence, [3,1,5,2,4]);
   assert.end();
 });
+
+test('identifies the last eliminated child as the winner', (assert) => {
+  const noOfChildren = 5;
+  const hopCount = 3;
+  const result = play({noOfChildren, hopCount});
+  assert.deepEqual(result.winner, 4);
+  assert.end();
+});
+
+function play(settings) {
+  return new Game().play(settings);
+}
