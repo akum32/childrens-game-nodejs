f65baa0618363204d254a7dcae78c49fd88c0707
Asheesh Karwal <asheesh.karwal@myob.com>
2017-04-17 12:14:23 +1000

    Fattens settings required to be passed to the game

--- tests/game-tests.js
+++ tests/game-tests.js
@@ -1,14 +1,14 @@
 const test = require('tape');
 const Game = require('../lib/game');
 
-test('eliminates children clockwise after the given number of hops', (assert) => {
-  const result = play({noOfChildren: 5, elimination: {hopCount: 3}});
+test('eliminates children clockwise after the given number of hops', assert => {
+  const result = play({noOfChildren: 5, hopCount: 3});
   assert.deepEqual(result.eliminatedSequence, [3,1,5,2,4]);
   assert.end();
 });
 
-test('identifies the last eliminated child as the winner', (assert) => {
-  const result = play({noOfChildren: 5, elimination: {hopCount: 3}});
+test('identifies the last eliminated child as the winner', assert => {
+  const result = play({noOfChildren: 5, hopCount: 3});
   assert.deepEqual(result.winner, 4);
   assert.end();
 });
