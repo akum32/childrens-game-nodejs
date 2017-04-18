6fa93f23859ac313dbd5d294d99ba44cc958e97a
Asheesh Karwal <asheesh.karwal@myob.com>
2017-04-18 21:44:20 +1000

    Renders more user friendly output

--- play.js
+++ play.js
@@ -3,4 +3,7 @@ const Game = require('./lib/game');
 
 const settings = parseArgs(process.argv.slice(2));
 const result = new Game().play(settings);
-console.log('Game finished!', result);
+
+console.log('Game finished!');
+console.log(`Winner: ${result.winner}`);
+console.log(`Eliminated sequence: ${result.eliminatedSequence}`);
